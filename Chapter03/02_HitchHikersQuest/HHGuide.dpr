a2735221757c49e12570996b169a087ef05a9394
dev@corneliusconcepts.com <corneliusdavid>
2020-06-25 17:37:04 -0700

    simple example of a console app and DLL that work on both Win64 and Linux

---
+++ Chapter03/02_HitchHikersQuest/HHGuide.dpr
@@ -0,0 +1,11 @@
+library HHGuide;
+
+function UniversalLifeAnswer: Integer;
+begin
+  Result := 42;
+end;
+
+exports UniversalLifeAnswer;
+
+begin
+end.
---
+++ Chapter03/02_HitchHikersQuest/HitchhikersQuest.dpr
@@ -0,0 +1,34 @@
+program HitchhikersQuest;
+{$APPTYPE CONSOLE}
+
+uses
+  {$IFDEF MSWINDOWS}
+  WinAPI.Windows,
+  {$ENDIF}
+  System.SysUtils;
+
+const
+  {$IFDEF MSWINDOWS}
+  LibName = 'HHGuide.dll';
+  {$ELSEIF DEFINED(LINUX)}
+  LibName = 'libHHGuide.so';
+  {$ENDIF}
+type
+  TUniversalLifeAnswerFunc = function: Integer; stdcall;
+var
+  DLLHandle: HModule;
+  LifesAnswer: TUniversalLifeAnswerFunc;
+begin
+  DLLHandle := LoadLibrary(LibName);
+
+  if DLLHandle = 0 then
+    Writeln('Could not load ' + LibName)
+  else begin
+    @LifesAnswer := GetProcAddress(DLLHandle, 'UniversalLifeAnswer');
+    Writeln('What is the answer to life?');
+    Writeln(LifesAnswer);
+    Readln;
+  end;
+
+  FreeLibrary(DLLHandle);
+end.
