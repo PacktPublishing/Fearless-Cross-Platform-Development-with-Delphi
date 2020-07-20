53b0d8cd8822afc583cffde0d2f1da448c7a2dfe
dev@corneliusconcepts.com <corneliusdavid>
2020-07-19 17:42:02 -0700

    added a repeat-until loop and a case statement with an else.

--- Chapter04/01_BasicSyntaxDemo/ControlDemo.dpr
+++ Chapter04/01_BasicSyntaxDemo/ControlDemo.dpr
@@ -1,14 +1,19 @@
 program ControlDemo;
 {$APPTYPE CONSOLE}
 
+uses
+  SysUtils;
+
 const
   MY_FAV_LANG = 'Delphi';
   OTHER_LANGUAGES: array[1..4] of string = ('C++', 'Java', 'Python', 'SQL');
 var
-  i: Integer;
   lang: string;
+  NumStr: string;
+  Num: Integer;
   Found: Boolean;
   MyLanguageName: string;
+  valid: Boolean;
 begin
   Write('What''s your favorite language? ');
   Readln(MyLanguageName);
@@ -27,6 +32,27 @@ begin
     if not Found then
       Writeln('I do not know about that language');
   end;
+
+  valid := False;
+  repeat
+    Writeln;
+    Write('Enter a number from 1 to 9: ');
+    Readln(NumStr);
+    if TryStrToInt(NumStr, Num) then begin
+      valid := True;
+      case Num of
+        1,2,3,4: Writeln('Your answer was in the bottom half of the range.');
+        5:       Writeln('Your answer was in the middle of the range.');
+        6..9:    Writeln('Your answer was in the upper half of the range.');
+      else
+        begin
+          Writeln('Your answer was outside of the requested range.');
+          valid := False;
+        end;
+      end;
+    end else
+      Writeln('Your answer could not be understood.');
+  until valid;
 
   Readln;
 end.
