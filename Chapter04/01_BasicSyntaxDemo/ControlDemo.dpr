cd6b82619862196f789d79e4861e1ed9a4c68ed9
dev@corneliusconcepts.com <corneliusdavid>
2020-07-19 17:38:33 -0700

    added simple for-loop in the else clause

--- Chapter04/01_BasicSyntaxDemo/ControlDemo.dpr
+++ Chapter04/01_BasicSyntaxDemo/ControlDemo.dpr
@@ -3,7 +3,10 @@ program ControlDemo;
 
 const
   MY_FAV_LANG = 'Delphi';
+  OTHER_LANGUAGES: array[1..4] of string = ('C++', 'Java', 'Python', 'SQL');
 var
+  i: Integer;
+  Found: Boolean;
   MyLanguageName: string;
 begin
   Write('What''s your favorite language? ');
@@ -12,8 +15,16 @@ begin
   if MyLanguageName = MY_FAV_LANG then
     Writeln('Really? That''s mine, too!')
   else begin
-    Writeln('Cool!  Mine is ' + MY_FAV_LANG);
-    Writeln('You should try ' + MY_FAV_LANG);
+    Found := False;
+    for i := 1 to 4 do
+      if MyLanguageName = OTHER_LANGUAGES[i] then begin
+        Writeln('That''s a good language');
+        Found := True;
+        Break;
+      end;
+
+    if not Found then
+      Writeln('I do not know about that language');
   end;
 
   Readln;
