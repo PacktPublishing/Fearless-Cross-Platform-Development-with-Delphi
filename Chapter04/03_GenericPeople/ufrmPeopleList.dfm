b39922835b628b01417f13d18232465f8c5f8b41
dev@corneliusconcepts.com <corneliusdavid>
2020-08-03 00:58:59 -0700

    First iteration of using generics to hold TPerson objects with TComparer sorting

---
+++ Chapter04/03_GenericPeople/ufrmPeopleList.dfm
@@ -0,0 +1,105 @@
+object frmPeopleList: TfrmPeopleList
+  Left = 0
+  Top = 0
+  Caption = 'People List'
+  ClientHeight = 340
+  ClientWidth = 515
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -13
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  DesignSize = (
+    515
+    340)
+  PixelsPerInch = 96
+  TextHeight = 16
+  object Label1: TLabel
+    Left = 290
+    Top = 8
+    Width = 46
+    Height = 16
+    Caption = 'Sort by:'
+  end
+  object lbPeople: TListBox
+    Left = 8
+    Top = 8
+    Width = 241
+    Height = 324
+    Anchors = [akLeft, akTop, akBottom]
+    TabOrder = 0
+  end
+  object grpNewPerson: TGroupBox
+    Left = 271
+    Top = 95
+    Width = 218
+    Height = 237
+    Caption = 'New Person'
+    TabOrder = 1
+    object Label3: TLabel
+      Left = 19
+      Top = 134
+      Width = 76
+      Height = 16
+      Caption = 'Date of Birth:'
+    end
+    object edtNewPersonFirstName: TLabeledEdit
+      Left = 19
+      Top = 56
+      Width = 172
+      Height = 24
+      EditLabel.Width = 96
+      EditLabel.Height = 16
+      EditLabel.Caption = 'New First Name:'
+      MaxLength = 40
+      TabOrder = 0
+    end
+    object edtNewPersonLastName: TLabeledEdit
+      Left = 19
+      Top = 104
+      Width = 172
+      Height = 24
+      EditLabel.Width = 94
+      EditLabel.Height = 16
+      EditLabel.Caption = 'New Last Name:'
+      MaxLength = 40
+      TabOrder = 1
+    end
+    object edtNewDOB: TDateTimePicker
+      Left = 19
+      Top = 156
+      Width = 116
+      Height = 24
+      Date = 44045.000000000000000000
+      Format = 'yyyy-MM-dd'
+      Time = 0.815059305554314100
+      TabOrder = 2
+    end
+    object btnAdd: TButton
+      Left = 19
+      Top = 192
+      Width = 75
+      Height = 25
+      Caption = '&Add'
+      TabOrder = 3
+      OnClick = btnAddClick
+    end
+  end
+  object cmbPersonSort: TComboBox
+    Left = 290
+    Top = 30
+    Width = 145
+    Height = 24
+    Style = csDropDownList
+    TabOrder = 2
+    OnChange = cmbPersonSortChange
+    Items.Strings = (
+      'First Name'
+      'Last Name'
+      'Date of Birth')
+  end
+end
---
+++ Chapter04/03_GenericPeople/ufrmPeopleList.pas
@@ -0,0 +1,169 @@
+unit ufrmPeopleList;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ExtCtrls,
+  System.Generics.Collections, System.Generics.Defaults;
+
+type
+  TfrmPeopleList = class(TForm)
+    lbPeople: TListBox;
+    grpNewPerson: TGroupBox;
+    edtNewPersonFirstName: TLabeledEdit;
+    edtNewPersonLastName: TLabeledEdit;
+    Label3: TLabel;
+    edtNewDOB: TDateTimePicker;
+    btnAdd: TButton;
+    cmbPersonSort: TComboBox;
+    Label1: TLabel;
+    procedure btnAddClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure cmbPersonSortChange(Sender: TObject);
+  private
+    type
+      TPerson = class
+      private
+        FFirstName: string;
+        FLastName: string;
+        FDateOfBirth: TDate;
+      public
+        constructor Create(const NewFN, NewLN: string; const NewDOB: TDate);
+        function Age: Integer;
+        property FirstName: string read FFirstName write FFirstName;
+        property LastName: string read FLastName write FLastName;
+        property DateOfBirth: TDate read FDateOfBirth write FDateOfBirth;
+      end;
+      TLastNameComparer = class(TComparer<TPerson>)
+        function Compare(const Left, Right: TPerson): Integer; override;
+      end;
+      TFirstNameComparer = class(TComparer<TPerson>)
+        function Compare(const Left, Right: TPerson): Integer; override;
+      end;
+      TBirthDateNameComparer = class(TComparer<TPerson>)
+        function Compare(const Left, Right: TPerson): Integer; override;
+      end;
+    var
+      FPeopleList: TList<TPerson>;
+    procedure AddPersonToList(ANewPerson: TPerson);
+    procedure ListPeople;
+  end;
+
+var
+  frmPeopleList: TfrmPeopleList;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  System.DateUtils;
+
+{ TfrmPeopleList.TLastNameComparer }
+
+function TfrmPeopleList.TLastNameComparer.Compare(const Left, Right: TPerson): Integer;
+begin
+  Result := CompareText(Left.LastName, Right.LastName);
+end;
+
+{ TfrmPeopleList.TFirstNameComparer }
+
+function TfrmPeopleList.TFirstNameComparer.Compare(const Left, Right: TPerson): Integer;
+begin
+  Result := CompareText(Left.FirstName, Right.FirstName);
+end;
+
+{ TfrmPeopleList.TBirthDateNameComparer }
+
+function TfrmPeopleList.TBirthDateNameComparer.Compare(const Left, Right: TPerson): Integer;
+begin
+  Result := CompareDate(Left.DateOfBirth, Right.DateOfBirth);
+end;
+
+{ TfrmPeopleList.TPerson }
+
+procedure TfrmPeopleList.FormCreate(Sender: TObject);
+begin
+  FPeopleList := TList<TPerson>.Create;
+
+  // starter data
+  FPeopleList.Add(TPerson.Create('Bob', 'Brothers', EncodeDate(1945, 7, 15)));
+  FPeopleList.Add(TPerson.Create('Sally', 'Sampson', EncodeDate(1977, 3, 20)));
+  FPeopleList.Add(TPerson.Create('Fred', 'Thimsfrabble', EncodeDate(1990, 8, 3)));
+  FPeopleList.Add(TPerson.Create('Zeek', 'Anderson', EncodeDate(1960, 11, 19)));
+  FPeopleList.Add(TPerson.Create('Alfred', 'Wilson', EncodeDate(1985, 9, 5)));
+
+  ListPeople;
+end;
+
+procedure TfrmPeopleList.FormDestroy(Sender: TObject);
+begin
+  FPeopleList.Free;
+end;
+
+function TfrmPeopleList.TPerson.Age: Integer;
+begin
+  Result := YearsBetween(Date, FDateOfBirth);
+end;
+
+constructor TfrmPeopleList.TPerson.Create(const NewFN, NewLN: string; const NewDOB: TDate);
+begin
+  FFirstName := NewFN;
+  FLastName  := NewLN;
+  FDateOfBirth := NewDOB;
+end;
+
+procedure TfrmPeopleList.AddPersonToList(ANewPerson: TPerson);
+begin
+  FPeopleList.Add(ANewPerson);
+end;
+
+procedure TfrmPeopleList.ListPeople;
+var
+  APerson: TPerson;
+  SortedList: TList<TPerson>;
+begin
+  case cmbPersonSort.ItemIndex of
+    0: SortedList := TList<TPerson>.Create(TFirstNameComparer.Create);
+    1: SortedList := TList<TPerson>.Create(TLastNameComparer.Create);
+    2: SortedList := TList<TPerson>.Create(TBirthDateNameComparer.Create);
+  else
+    SortedList := TList<TPerson>.Create;  // no sorting
+  end;
+
+  lbPeople.Items.Clear;
+  for APerson in FPeopleList do
+    SortedList.Add(APerson);
+
+  SortedList.Sort;
+  for APerson in SortedList do
+    lbPeople.Items.Add(Format('%s %s, Age: %d',
+               [APerson.FirstName, APerson.LastName, APerson.Age]));
+end;
+
+procedure TfrmPeopleList.btnAddClick(Sender: TObject);
+var
+  NewPerson: TPerson;
+begin
+  if (Length(edtNewPersonFirstName.Text) > 0) and
+     (Length(edtNewPersonLastName.Text) > 0) then begin
+    NewPerson := TPerson.Create(edtNewPersonFirstName.Text,
+                                edtNewPersonLastName.Text,
+                                edtNewDOB.Date);
+    AddPersonToList(NewPerson);
+    ListPeople;
+
+    edtNewPersonFirstName.Text := EmptyStr;
+    edtNewPersonLastName.Text  := EmptyStr;
+  end else
+    ShowMessage('Please fill in both names.');
+end;
+
+procedure TfrmPeopleList.cmbPersonSortChange(Sender: TObject);
+begin
+  ListPeople;
+end;
+
+end.
