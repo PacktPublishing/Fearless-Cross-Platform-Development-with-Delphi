68c8a51c12263d1e5447651d724ce1bb11fee628
dev@corneliusconcepts.com <corneliusdavid>
2020-08-04 20:06:51 -0700

    refactored GetPropertyValueAsString

--- Chapter04/05_RttiExplorer/ufrmRttiMain.pas
+++ Chapter04/05_RttiExplorer/ufrmRttiMain.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ExtCtrls,
+  System.Rtti, System.TypInfo;
 
 type
   TfrmRttiMain = class(TForm)
@@ -20,6 +21,8 @@ type
     btnOK: TButton;
     procedure ComponentClick(Sender: TObject);
     procedure cbCheckSampleClick(Sender: TObject);
+  private
+    function GetPropertyValueAsString(Sender: TObject; LProperty: TRttiProperty): string;
   end;
 
 var
@@ -29,9 +32,6 @@ implementation
 
 {$R *.dfm}
 
-uses
-  System.Rtti, System.TypInfo;
-
 { TfrmRttiMain }
 
 procedure TfrmRttiMain.cbCheckSampleClick(Sender: TObject);
@@ -40,13 +40,31 @@ begin
   ComponentClick(Sender);
 end;
 
+function TfrmRttiMain.GetPropertyValueAsString(Sender: TObject; LProperty: TRttiProperty): string;
+var
+  LValue: TValue;
+begin
+  LValue := LProperty.GetValue(Sender);
+  case LProperty.PropertyType.TypeKind of
+    tkString, tkLString, tkUString:
+      Result := LProperty.GetValue(Sender).AsString;
+    tkInt64, tkInteger:
+      Result := IntToStr(LProperty.GetValue(Sender).AsInteger);
+    tkFloat:
+      Result := FloatToStr(LProperty.GetValue(Sender).AsExtended);
+    tkEnumeration:
+      Result := GetEnumName(LProperty.PropertyType.Handle, LValue.AsOrdinal);
+  else
+    Result := 'Unsupported type: ' + LProperty.PropertyType.Name;
+  end;
+end;
+
 procedure TfrmRttiMain.ComponentClick(Sender: TObject);
 var
   LContext: TRttiContext;
   LRttiType: TRttiType;
   LProperty: TRttiProperty;
   LStrValue: string;
-  LValue: TValue;
   LPropEntry: string;
 begin
   lbRttiInfo.Items.Clear;
@@ -58,19 +76,7 @@ begin
 
     for LProperty in LRttiType.GetProperties do
       if LProperty.PropertyType.TypeKind in ([tkUString, tkString, tkLString, tkInteger, tkInt64, tkEnumeration, tkFloat]) then begin
-        LValue := LProperty.GetValue(Sender);
-        case LProperty.PropertyType.TypeKind of
-          tkString,
-          tkLString,
-          tkUString: LStrValue := LProperty.GetValue(Sender).AsString;
-          tkInt64,
-          tkInteger: LStrValue := IntToStr(LProperty.GetValue(Sender).AsInteger);
-          tkFloat:   LStrValue := FloatToStr(LProperty.GetValue(Sender).AsExtended);
-          tkEnumeration: LStrValue := GetEnumName(LProperty.PropertyType.Handle, LValue.AsOrdinal);
-          tkDynArray: LStrValue := 'Array[' + InttoStr(LValue.GetArrayLength) + ']';
-        else
-          LStrValue := 'Unsupported type: ' + LProperty.PropertyType.Name;
-        end;
+        LStrValue := GetPropertyValueAsString(Sender, LProperty);
 
         LPropEntry := LProperty.Name + ': ' + LProperty.PropertyType.Name + ' = ' + LStrValue;
         if lbRttiInfo.Items.IndexOf(LPropEntry) = -1 then
