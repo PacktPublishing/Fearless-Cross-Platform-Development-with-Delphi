a2848c7577f22d0463d7e1055219f1de455312b6
dev@corneliusconcepts.com <corneliusdavid>
2020-10-11 21:22:01 -0700

    reworked example with different field names

--- Chapter06/02_WizardBound/ufrmPrototypedHires.pas
+++ Chapter06/02_WizardBound/ufrmPrototypedHires.pas
@@ -9,29 +9,35 @@ uses
   System.Rtti, System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.Controls,
   FMX.EditBox, FMX.NumberBox, FMX.StdCtrls, FMX.Objects, FMX.DateTimeCtrls,
   FMX.Layouts, Fmx.Bind.Navigator, Data.Bind.Components,
-  FMX.Controls.Presentation, FMX.Edit, Data.Bind.ObjectScope, Data.Bind.Grid;
+  FMX.Controls.Presentation, FMX.Edit, Data.Bind.ObjectScope, Data.Bind.Grid,
+  Fmx.Bind.Grid, FMX.Grid.Style, FMX.ScrollBox, FMX.Grid;
 
 type
   TForm1 = class(TForm)
-    BindingsList1: TBindingsList;
-    PrototypeBindSource1: TPrototypeBindSource;
     EditContactTitle1: TEdit;
     LabelContactTitle1: TLabel;
-    LinkControlToFieldContactTitle1: TLinkControlToField;
     NavigatorPrototypeBindSource1: TBindNavigator;
     DateEditDateTimeField1: TDateEdit;
     LabelDateTimeField1: TLabel;
-    LinkControlToFieldDateTimeField1: TLinkControlToField;
     EditContactName1: TEdit;
     LabelContactName1: TLabel;
-    LinkControlToFieldContactName1: TLinkControlToField;
     ImageContactBitmap1: TImage;
-    LinkControlToFieldContactBitmap1: TLinkControlToField;
     CheckBoxBoolField1: TCheckBox;
-    LinkControlToFieldBoolField1: TLinkControlToField;
     NumberBoxIntField1: TNumberBox;
     LabelIntField1: TLabel;
-    LinkControlToFieldIntField1: TLinkControlToField;
+    BindingsList1: TBindingsList;
+    PrototypeBindSource1: TPrototypeBindSource;
+    LinkPropertyToFieldTitle: TLinkPropertyToField;
+    LinkControlToField1: TLinkControlToField;
+    LinkControlToField2: TLinkControlToField;
+    LinkControlToField3: TLinkControlToField;
+    LinkControlToField4: TLinkControlToField;
+    LinkControlToField5: TLinkControlToField;
+    GridPrototypeBindSource1: TGrid;
+    LinkGridToDataSourcePrototypeBindSource12: TLinkGridToDataSource;
+    Rectangle1: TRectangle;
+    lblContactName: TLabel;
+    LinkPropertyToFieldContactName: TLinkPropertyToField;
   private
     { Private declarations }
   public
