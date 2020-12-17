4d7f83212e6935f55e7a35d121824e8bf1f37e2c
David Cornelius <dev@corneliusconcepts.com>
2020-12-16 17:16:16 -0800

    3D demo - added bulbs, a star, and texture to transform the cone into a Christmas tree, duplicated it; changed some colors and materials and added animation

--- Chapter08/01_Quadrants/ufrmQaudrants.pas
+++ Chapter08/01_Quadrants/ufrmQaudrants.pas
@@ -6,20 +6,59 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms3D, FMX.Types3D, FMX.Forms, FMX.Graphics, 
   FMX.Dialogs, FMX.Objects, System.Math.Vectors, FMX.Objects3D, FMX.Controls3D,
-  FMX.MaterialSources;
+  FMX.MaterialSources, FMX.Ani;
 
 type
   Tfrm3DQuadrants = class(TForm3D)
     Cube1: TCube;
-    Cone1: TCone;
+    ConeChristmasTree: TCone;
     PlaneVertical: TPlane;
     PlaneHorizontal: TPlane;
-    Sphere1: TSphere;
+    SphereGlobe: TSphere;
     TextureMaterialSourceGlobe: TTextureMaterialSource;
     LightMaterialSourceBlue: TLightMaterialSource;
-    Cube2: TCube;
     Light1: TLight;
-    Text3D1: TText3D;
+    txtQuadrant: TText3D;
+    FloatAnimation1: TFloatAnimation;
+    LightMaterialSourceGreenery: TLightMaterialSource;
+    ColorMaterialSourceGray: TColorMaterialSource;
+    ColorMaterialSourceBlack: TColorMaterialSource;
+    ColorAnimation1: TColorAnimation;
+    bulb7: TSphere;
+    bulb8: TSphere;
+    bulb1: TSphere;
+    bulb2: TSphere;
+    bulb3: TSphere;
+    bulb4: TSphere;
+    bulb5: TSphere;
+    bulb6: TSphere;
+    path3dStar: TPath3D;
+    ColorAnimationStarGlow: TColorAnimation;
+    ColorMaterialStar: TColorMaterialSource;
+    Cone1: TCone;
+    Sphere1: TSphere;
+    Sphere2: TSphere;
+    Sphere3: TSphere;
+    Sphere4: TSphere;
+    Sphere5: TSphere;
+    Sphere6: TSphere;
+    Sphere7: TSphere;
+    Sphere8: TSphere;
+    Path3D1: TPath3D;
+    ColorMaterialSource1: TColorMaterialSource;
+    ColorAnimation2: TColorAnimation;
+    Cone2: TCone;
+    Sphere9: TSphere;
+    Sphere10: TSphere;
+    Sphere11: TSphere;
+    Sphere12: TSphere;
+    Sphere13: TSphere;
+    Sphere14: TSphere;
+    Sphere15: TSphere;
+    Sphere16: TSphere;
+    Path3D2: TPath3D;
+    ColorMaterialSource2: TColorMaterialSource;
+    ColorAnimation3: TColorAnimation;
   private
     { Private declarations }
   public
