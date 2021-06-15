f31a054ba1758c3b9fc4ab25c7577a190d17034d
dev@corneliusconcepts.com <corneliusdavid>
2021-06-15 00:22:58 -0700

    ISAPI web module

---
+++ Chapter13/02_MyParksISAPI/uwmMyParks.pas
@@ -0,0 +1,179 @@
+unit uwmMyParks;
+
+interface
+
+uses
+  System.SysUtils, System.StrUtils, System.Classes, Web.HTTPApp, Web.HTTPProd, Web.DSProd,
+  FireDAC.UI.Intf, FireDAC.FMXUI.Wait, FireDAC.Stan.Intf, FireDAC.Comp.UI, Web.DBWeb;
+
+type
+  TwmMyParks = class(TWebModule)
+    ppAbout: TPageProducer;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    dstpMyParks: TDataSetTableProducer;
+    ppGetParkQuery: TPageProducer;
+    ppShowParkFromCoords: TPageProducer;
+    ppPageHeader: TPageProducer;
+    ppPageFooter: TPageProducer;
+    ppMenu: TPageProducer;
+    ppParkList: TPageProducer;
+    procedure ppStandardHTMLTag(Sender: TObject; Tag: TTag; const TagString: string; TagParams: TStrings;
+      var ReplaceText: string);
+    procedure ppShowParkFromCoordsHTMLTag(Sender: TObject; Tag: TTag; const TagString: string; TagParams: TStrings;
+      var ReplaceText: string);
+    procedure wmMyParkswaiShowParkFromCoordsAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse;
+      var Handled: Boolean);
+    procedure WebModuleCreate(Sender: TObject);
+    procedure ppPageHeaderHTMLTag(Sender: TObject; Tag: TTag; const TagString: string; TagParams: TStrings;
+      var ReplaceText: string);
+    procedure wmMyParkswaiDefaultAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse;
+      var Handled: Boolean);
+  private
+    FPageTitle: string;
+    FLongitude, FLatitude: Double;
+    FParkName: string;
+    function CheckFooter(const TagString: string): string;
+    function CheckAppName(const TagString: string): string;
+    function GetMenu(CurrPageProducer: TPageProducer): string;
+  end;
+
+var
+  WebModuleClass: TComponentClass = TwmMyParks;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+uses
+  LoggerPro,
+  LoggerPro.FileAppender,
+  uMyParksLogging,
+  udmParksDB;
+
+{$R *.dfm}
+
+const
+  APP_NAME = 'My Parks';
+
+
+function TwmMyParks.CheckAppName(const TagString: string): string;
+begin
+  if SameText(TagString, 'AppName') then
+    Result := APP_NAME
+  else
+    Result := EmptyStr;
+end;
+
+procedure TwmMyParks.ppPageHeaderHTMLTag(Sender: TObject; Tag: TTag; const TagString: string; TagParams: TStrings;
+  var ReplaceText: string);
+begin
+  ReplaceText := CheckAppName(TagString);
+  if ReplaceText.IsEmpty then
+    if SameText(TagString, 'PageTitle') then
+      ReplaceText := FPageTitle
+    else
+      ReplaceText := EmptyStr;
+end;
+
+function TwmMyParks.CheckFooter(const TagString: string): string;
+begin
+  if SameText(TagString, 'Footer') then
+    Result := ppPageFooter.Content
+  else
+    Result := EmptyStr;
+end;
+
+function TwmMyParks.GetMenu(CurrPageProducer: TPageProducer): string;
+const
+  UL_START     = '<ul class="nav nav-pills">';
+  LI_START     = '<li class="nav-item">';
+  ABOUT_FMT    = '<a class="nav-link %s" href="about">About</a>';
+  PARKLIST_FMT = '<a class="nav-link %s" href="parklist">List Parks</a>';
+  GETPARK_FMT  = '<a class="nav-link %s" href="getpark">Get Park Name</a>';
+  LI_END       = '</li>';
+  UL_END       = '</ul>';
+
+  function NavLink(FormatText: string; Test: Boolean): string; inline;
+  begin
+    Result := LI_START + Format(FormatText, [IfThen(Test, 'active', '')]) + LI_END;
+  end;
+
+begin
+ Result :=
+    UL_START +
+    NavLink(ABOUT_FMT,   CurrPageProducer = ppAbout) +
+    NavLink(GETPARK_FMT, CurrPageProducer = ppGetParkQuery) +
+    NavLink(PARKLIST_FMT, CurrPageProducer = ppParkList) +
+    UL_END;
+end;
+
+procedure TwmMyParks.ppStandardHTMLTag(Sender: TObject; Tag: TTag; const TagString: string;
+  TagParams: TStrings; var ReplaceText: string);
+begin
+  if SameText(TagString, 'Header') then begin
+    if (TagParams.Count = 1) then begin
+      FPageTitle := TagParams.Values['PageTitle'];
+      ReplaceText := ppPageHeader.Content;
+    end;
+  end else if SameText(TagString, 'Menu') then
+    ReplaceText := GetMenu(Sender as TPageProducer)
+  else if SameText(TagString, 'Footer') then
+    ReplaceText := ppPageFooter.Content;
+
+  // only for Park List
+  if ReplaceText.IsEmpty and SameText(TagString, 'parklist') then
+    ReplaceText := dstpMyParks.Content;
+end;
+
+procedure TwmMyParks.ppShowParkFromCoordsHTMLTag(Sender: TObject; Tag: TTag; const TagString: string;
+  TagParams: TStrings; var ReplaceText: string);
+begin
+  if SameText(TagString, 'Header') then begin
+    if (TagParams.Count = 1) then begin
+      FPageTitle := TagParams.Values['PageTitle'];
+      ReplaceText := ppPageHeader.Content;
+    end;
+  end else if SameText(TagString, 'Menu') then
+    ReplaceText := GetMenu(Sender as TPageProducer)
+  else
+    ReplaceText := CheckFooter(TagString);
+
+  if ReplaceText.IsEmpty then
+    if SameText(TagString, 'longitude') then
+      ReplaceText := FLongitude.ToString
+    else if SameText(TagString, 'latitude') then
+      ReplaceText := FLatitude.ToString
+    else if SameText(TagString, 'ParkName') then
+      ReplaceText := FParkName;
+end;
+
+procedure TwmMyParks.wmMyParkswaiDefaultAction(Sender: TObject; Request: TWebRequest; Response: TWebResponse;
+  var Handled: Boolean);
+begin
+  Response.SendRedirect(ExtractFileName(WebApplicationFileName) + '/about');
+end;
+
+procedure TwmMyParks.wmMyParkswaiShowParkFromCoordsAction(Sender: TObject;
+  Request: TWebRequest; Response: TWebResponse; var Handled: Boolean);
+begin
+  if Request.QueryFields.Count = 2 then begin
+    var long, lat: Double;
+    if TryStrToFloat(Request.QueryFields.Values['long'] , long) and
+       TryStrToFloat(Request.QueryFields.Values['lat'], lat) then begin
+      FLongitude := long;
+      FLatitude := lat;
+      var ParkInfo := dmParksDB.LookupParkByLocation(FLongitude, FLatitude);
+      FParkName := ParkInfo.ParkName;
+    end;
+  end;
+
+  Response.Content := ppShowParkFromCoords.Content;
+end;
+
+procedure TwmMyParks.WebModuleCreate(Sender: TObject);
+begin
+  // disable logging
+  Log := BuildLogWriter([]);
+end;
+
+end.
